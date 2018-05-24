041a252055ce3d13b7d70b292055d79cb6377b49
Massimo Dalmazzi <massimodalmazzi@MacBook-Air-di-Massimo.local>
2018-05-24 08:58:36 +0200

    Change IP per development

--- application.js
+++ application.js
@@ -21,7 +21,9 @@ var app = express();
 //mongoose.connect('dea-lab-user:pwd-dea-lab@ds161793.mlab.com:61793/dea-lab');
 //mongoose.connect('localhost:27017/bibblo');
 //mongoose.connect('localhost:27017/testYOUTube');
-mongoose.connect('localhost:27017/SpaggiariPage');
+//mongoose.connect('localhost:27017/SpaggiariPage');
+
+mongoose.connect('admin:abc124@localhost/SpaggiariPage');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
--- assets/app/user/user.service.ts
+++ assets/app/user/user.service.ts
@@ -13,7 +13,9 @@ export class UserService {
 
    //private path_to_server: string = 'http://localhost:4200';
 
-   private path_to_server: string = 'http://192.168.1.139:3000';
+   //private path_to_server: string = 'http://192.168.1.139:3000';
+
+   private path_to_server: string = 'http://213.26.180.210:3000';
 
 
    ///
