8b8cb647ce7704235bf70ba3aaef0ae3e3dc7232
Massimo Dalmazzi <massimodalmazzi@MacBook-Air-di-Massimo.local>
2018-06-20 13:12:26 +0200

    Minor changes

--- routes/crawler.js
+++ routes/crawler.js
@@ -260,7 +260,7 @@ router.patch('/:id', function(req, res, next) {
         }
         console.log('Risposta crawler: ', message, message[0].name, req.params.id);
 
-        if (message[0].progetti) {
+        if (message[0].progetti.lenght != 0) {
             message[0].progetti.push(req.params.id);
             console.log('PUSH');
         } else {
