ff6d97690324f684d254785456117c8fe560e640
Massimo Dalmazzi <massimodalmazzi@MacBook-Air-di-Massimo.local>
2018-06-29 11:49:16 +0200

    Image 15000

--- routes/images.js
+++ routes/images.js
@@ -228,7 +228,7 @@ router.get('/:word/:scuola/:risorsa/:fonte/:materia/:licenza', function(req, res
         words = messages;
 
 
-        for (var i = 10001; i < 30000; i++) {
+        for (var i = 10001; i < 15000; i++) {
             //console.log('i: ', i, words[i].titolo, words[i].images.length);
             if (words[i].images.length == 0) {
 
