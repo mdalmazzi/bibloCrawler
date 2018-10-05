126575230e88f9668169b98dcd6d99eee23ed789
Massimo Dalmazzi <massimodalmazzi@air-di-massimo.homenet.telecomitalia.it>
2018-10-05 11:53:15 +0200

    Ri-inserito peso search

--- routes/postloginf.js
+++ routes/postloginf.js
@@ -186,7 +186,7 @@ router.get('/:word/:scuola/:risorsa/:fonte/:materia/:licenza', function(req, res
                         })
                     }
                 })
-            // .sort({ score: { $meta: 'textScore' } })
+            .sort({ score: { $meta: 'textScore' } })
             .limit(20)
 
         //.sort({ quality: -1 })
