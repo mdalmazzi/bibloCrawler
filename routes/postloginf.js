be4d699e43579ee2c8276f0796f7323c22b688e3
Massimo Dalmazzi <massimodalmazzi@air-di-massimo.homenet.telecomitalia.it>
2018-10-06 10:13:33 +0200

    no peso a search text

--- routes/postloginf.js
+++ routes/postloginf.js
@@ -187,7 +187,7 @@ router.get('/:word/:scuola/:risorsa/:fonte/:materia/:licenza', function(req, res
                         })
                     }
                 })
-            .sort({ score: { $meta: 'textScore' } })
+            // .sort({ score: { $meta: 'textScore' } })
             .limit(25)
 
 
