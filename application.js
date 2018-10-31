fa4bed0f31e70b483fb9deac65ad27300a2c0a16
Massimo Dalmazzi <massimodalmazzi@air-di-massimo.homenet.telecomitalia.it>
2018-10-31 10:12:18 +0100

    back to local for db

--- application.js
+++ application.js
@@ -28,8 +28,8 @@ var app = express();
 //mongoose.connect('localhost:27017/testYOUTube');
 // var options = { server: { socketOptions: { connectTimeoutMS: 100000 } } };
 
-// mongoose.connect('localhost:27017/SpaggiariPage');
-mongoose.connect('http://192.168.140.44:27017/SpaggiariPage');
+mongoose.connect('localhost:27017/SpaggiariPage');
+// mongoose.connect('http://192.168.140.44:27017/SpaggiariPage');
 
 // mongoose.connect('localhost:27017/SpaggiariPage', {
 //     server: {
