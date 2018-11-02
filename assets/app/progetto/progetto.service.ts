a68ff6ce138eeba41d608a20156df8fe454d8754
Massimo Dalmazzi <massimodalmazzi@air-di-massimo.homenet.telecomitalia.it>
2018-11-02 11:58:20 +0100

    TRy for Internal IP

--- assets/app/progetto/progetto.service.ts
+++ assets/app/progetto/progetto.service.ts
@@ -23,8 +23,8 @@ export class ProgettoService {
     public progetto: Progetto;
 
     // private path_to_server: string = 'http://localhost:3000';
-    private path_to_server: string = 'http://localhost:8880';
-    // private path_to_server: string = 'http://192.168.140.44:8880';
+    // private path_to_server: string = 'http://localhost:8880';
+    private path_to_server: string = 'http://192.168.140.44:8880';
   
   
     constructor(private http: Http) {}
--- assets/app/user/user.service.ts
+++ assets/app/user/user.service.ts
@@ -10,8 +10,8 @@ import {User} from "./user.model";
 export class UserService {
 
 //    private path_to_server: string = 'http://localhost:3000';
-   private path_to_server: string = 'http://localhost:8880';
-//    private path_to_server: string = 'http://192.168.140.44:8880';
+//    private path_to_server: string = 'http://localhost:8880';
+   private path_to_server: string = 'http://192.168.140.44:8880';
   
     constructor(private http: Http) {}
 
